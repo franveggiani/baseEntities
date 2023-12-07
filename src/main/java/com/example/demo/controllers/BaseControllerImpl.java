286b87318b526a9a799900e550df7a5d832fbbd0
franveggiani <francoveggiani@gmail.com>
2023-12-07 20:26:15 -0300

    first

--- src/main/java/com/example/demo/controllers/BaseControllerImpl.java
+++ src/main/java/com/example/demo/controllers/BaseControllerImpl.java
@@ -1,15 +1,12 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.Base;
-import com.example.demo.entities.Persona;
 import com.example.demo.services.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.Serializable;
 
 public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceImpl<E,Long>> implements BaseController<E, Long> {
     @Autowired
